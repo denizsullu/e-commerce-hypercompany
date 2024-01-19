3c71e4dc0a7e56ba9eaf653d702f5d534eb48485
deniz <denizsullu@protonmail.com>
2024-01-19 21:10:41 +0300

    Add product detail page, category selection, and optimize images for speed

---
+++ e-commerce-frontend/src/app/components/product-main/product-main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-main.component.html',
+  styleUrl: './product-main.component.scss'
+})
+export class ProductMainComponent {
+
+}
---
+++ e-commerce-frontend/src/app/components/sidebar-category/sidebar-category.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-category',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidebar-category.component.html',
+  styleUrl: './sidebar-category.component.scss'
+})
+export class SidebarCategoryComponent {
+
+}
