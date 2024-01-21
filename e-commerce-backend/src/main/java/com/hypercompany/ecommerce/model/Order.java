507c7fad211d599afda5de0378cc11524104b672
deniz <denizsullu@protonmail.com>
2024-01-21 17:22:13 +0300

    Created the User, Address, Cart, and Order entities to manage user information, addresses, shopping carts, and orders. Implemented relationships between these entities for a comprehensive user management system. Additional features and validations will be added in subsequent commits

---
+++ e-commerce-backend/src/main/java/com/hypercompany/ecommerce/model/Order.java
@@ -0,0 +1,28 @@
+package com.hypercompany.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "order")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String orderDate;
+    private Double totalAmount;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+}
